f1ed771213fcdd5d76ef4fb2674359c4364aec18
sixpeteunder <plenjo15@gmail.com>
2021-05-03 23:19:37 +0300

    Init Jetstream

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'              => 'user',
+            'email'             => 'user@mail.com',
+            'email_verified_at' => now(),
+            'password'          => Hash::make('password'),
+        ]);
+    }
+}
