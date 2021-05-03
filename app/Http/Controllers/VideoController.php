2ba1ecdf5b95be90ec4a5ee3f67473cc52125aa2
sixpeteunder <plenjo15@gmail.com>
2021-05-04 01:54:20 +0300

    Implement backend call logic

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\AgoraDynamicKey\RtcTokenBuilder;
+use App\Events\MakeVideoCall;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,16 +13,33 @@ class VideoController extends Controller
     /**
      * Get a token.
      */
-    public function token()
+    public function token(Request $request)
     {
+        $appID = config('agora.app_id');
+        $appCertificate = config('agora.app_certificate');
+        $expireTimeInSeconds = config('agora.expire_time');
 
+        return RtcTokenBuilder::buildTokenWithUserAccount(
+            $appID,
+            $appCertificate,
+            $request->channelName,
+            Auth::user()->name,
+            RtcTokenBuilder::RoleAttendee,
+            now()->getTimestamp() + $expireTimeInSeconds
+        );
     }
 
     /**
      * Call a user.
      */
-    public function call() 
+    public function call(Request $request)
     {
+        $videoCallEvent = new MakeVideoCall([
+            'userToCall' => $request->user_to_call,
+            'channelName' => $request->channel_name,
+            'from' => Auth::id()
+        ]);
 
+        broadcast($videoCallEvent)->toOthers();
     }
 }
